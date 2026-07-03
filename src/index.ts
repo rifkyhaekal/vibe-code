aeb2da1b4ab3feeb600ec0759746866e2416109c
Rifky Haekal Al-Fadillah <haekalhaekal001@gmail.com>
2026-07-03 19:01:55 +0700

    feat: implement user registration API

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Elysia } from "elysia";
+import { usersRoute } from "./routes/users-route";
 
 const app = new Elysia()
+  .use(usersRoute)
   .get("/", () => ({
     status: "ok",
     service: "Elysia + Drizzle + MySQL Backend",
